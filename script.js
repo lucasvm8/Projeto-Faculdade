6a725c2c13daea087a6e2711ed3b55582fe383cb
unknown <lucasvm8@gmail.com>
2021-03-20 15:03:46 -0300

    branch master

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
+const selectFile = document.querySelector('texto')
 
-let pdffile = fs.readFileSync('XP.pdf')
+let pdffile = fs.readFileSync(selectFile)
 
 pdf(pdffile).then(function (data){
     console.log(data.text)
