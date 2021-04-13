04c0637b21d264391ddcd1675340bec775435a60
unknown <lucasvm8@gmail.com>
2021-04-12 22:21:32 -0300

    api

--- api.js
+++ api.js
@@ -1,11 +1,13 @@
-let url = "https://significado.herokuapp.com/livro";
+
+let url = "https://significado.herokuapp.com/palavra";
+let method = "GET"
 
 let xhttp = new XMLHttpRequest();
-xhttp.open("GET", url);
+xhttp.open(method, url);
 xhttp.onreadystatechange = () => {
   if (xhttp.readyState == 4) {
     if (xhttp.status == 200) {
-      console.log(xhr.responseText);
+      console.log(xhttp.responseText);
     }
   }
 };
