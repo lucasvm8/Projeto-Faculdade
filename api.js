4443738bc26b750dc5864662f70b8ae1dfe04354
unknown <lucasvm8@gmail.com>
2021-04-14 20:35:37 -0300

    grande atualizaçao

--- api.js
+++ api.js
@@ -1,15 +1,28 @@
-
-let url = "https://significado.herokuapp.com/palavra";
-let method = "GET"
+let url = "https://significado.herokuapp.com/meanings/internet";
+let method = "GET";
 
 let xhttp = new XMLHttpRequest();
-xhttp.open(method, url);
+xhttp.open(method, url, true);
 xhttp.onreadystatechange = () => {
-  if (xhttp.readyState == 4) {
-    if (xhttp.status == 200) {
-      console.log(xhttp.responseText);
+  if (xhttp.readyState === 4) {
+    if (xhttp.status === 200) {
+      let classe = JSON.parse(xhttp.responseText);
+      console.log(classe[0].class.split(' ')[0]);
     }
   }
 };
 
 xhttp.send();
+
+let mhttp = new XMLHttpRequest();
+mhttp.open(method, url, true);
+mhttp.onreadystatechange = () => {
+  if (mhttp.readyState === 4) {
+    if (mhttp.status === 200) {
+      let desc = JSON.parse(mhttp.responseText);
+      console.log(desc[0].meanings[0]);
+    }
+  }
+};
+
+mhttp.send();
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const pdf = require("pdf-parse");
 
-let filePath = "uploads/file.pdf";
-let pdfFile = fs.readFileSync("uploads/file.pdf");
+let filePath = "file.pdf";
+let pdfFile = fs.readFileSync(filePath);
 
 async function readPdf() {
   try {
@@ -22,9 +22,30 @@ async function separarPalavras() {
   }
 }
 
-async function palavras() {
+async function quantidadeDeCaracteres() {
   try {
-    let data = await separarPalavras(filePath);
+    let data = await separarPalavras(pdfFile);
+    let array = [];
+
+    for (i in data) {
+      if (data[i].length > 3) {
+        array[i] = data[i];
+      }
+    }
+
+    let palavrasMaiorQueDois = array.filter(function (el) {
+      return el != null;
+    });
+
+    return palavrasMaiorQueDois;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function agruparPalavras() {
+  try {
+    let data = await quantidadeDeCaracteres(filePath);
     return data.reduce((acc, curr) => {
       acc[curr] = (acc[curr] || 0) + 1;
       return acc;
@@ -36,11 +57,11 @@ async function palavras() {
 
 async function topDezPalavras() {
   try {
-    let data = await palavras(filePath);
+    let data = await agruparPalavras(filePath);
     return console.log(
       Object.entries(data)
         .sort((a, b) => b[1] - a[1])
-        .slice(0, 10)
+        .slice(0, 20)
     );
   } catch (err) {
     console.log(err);
